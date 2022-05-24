0c27d085481e9449c2a9c5538f7633aa74f1a4b4
FrederikALP <fred05k3@stud.kea.dk>
2022-05-24 11:19:24 +0200

    fix s

--- Controllers/PublicProfileController.cs
+++ Controllers/PublicProfileController.cs
@@ -4,6 +4,12 @@ using CbsStudents.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 
 namespace cbsStudents.Controllers;
 
@@ -12,7 +18,14 @@ public class PublicProfileController : Controller
     private CbsStudentsContext _context;
     private readonly UserManager<IdentityUser> _userManager;
 
+    public PublicProfileController(CbsStudentsContext context, UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+        this._context = context;
+    }
 
+    // GET: Events
+    [AllowAnonymous]
     public IActionResult Index()
     {
         return View();
